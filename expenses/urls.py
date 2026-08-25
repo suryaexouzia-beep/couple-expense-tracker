3336c64d06654343d4cf14bba2bbecaca79ed433
suryaexouzia-beep <suryaexouzia@email.example.com>
2026-08-25 12:02:29 +0400

    setup admin

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-path("setup-admin/", views.setup_admin),
     # Dashboard
     path(
         "",
@@ -18,21 +16,18 @@ path("setup-admin/", views.setup_admin),
         name="expenses"
     ),
 
-    # Add Expense
     path(
         "expenses/add/",
         views.add_expense,
         name="add_expense"
     ),
 
-    # Add Category
     path(
         "category/add/",
         views.add_category,
         name="add_category"
     ),
 
-    # Delete Expense
     path(
         "expenses/delete/<int:expense_id>/",
         views.delete_expense,
@@ -46,18 +41,15 @@ path("setup-admin/", views.setup_admin),
         name="income"
     ),
 
-    # Add Income
     path(
         "income/add/",
         views.add_income,
         name="add_income"
     ),
 
-    # Delete Income
     path(
         "income/delete/<int:income_id>/",
         views.delete_income,
         name="delete_income"
     ),
-
 ]
